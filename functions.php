9b1ef536477ac66ed5fecd4e53706319834cc3ac
Jacek Jarczok <kson.eu@gmail.com>
2020-03-20 17:18:34 +0100

    Develop homepage

--- functions.php
+++ functions.php
@@ -10,4 +10,14 @@ function theme_enqueue_styles() {
       array( $parent_style )
   );
 }
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' ); 
+
+/*
+function bedegozjadl_files() {
+  wp_enqueue_style('custom_google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+}
+
+add_action('wp_enqueue_scripts', 'bedegozjadl_files');
+*/
