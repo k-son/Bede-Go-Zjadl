26951cd8ad040a054d5a4f5e1675998b0ac4b5da
Jacek Jarczok <kson.eu@gmail.com>
2020-03-23 16:48:15 +0100

    Style homepage

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ const difficultyLevelText = document.querySelectorAll('.difficulty-level__text')
 
 /// Difficulty level ///
 difficultyLevelText.forEach(el => {
+
   if (el.innerText == "TRUDNY") {
     el.parentElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.lastElementChild.style.fill = 'red';
   } else if (el.innerText == "ŚREDNI") {
     el.parentElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.lastElementChild.style.fill = 'orange';
   } else if (el.innerText == "ŁATWY") {
-    el.parentElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.lastElementChild.style.fill = 'green'; 
+    el.parentElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.lastElementChild.style.fill = 'green';
   }
 });
